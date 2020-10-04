0b14731643373923a868097a1147b2cb528bc5f5
Anuj Shukla <62193037+Atypical-introvert@users.noreply.github.com>
2020-10-04 20:34:03 +0530

    Add files via upload

---
+++ Random Color Generator/main.js
@@ -0,0 +1,11 @@
+function anujColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function anuj() {
+  $("body").css("background-color", anujColor());
+}
